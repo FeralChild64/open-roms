9c3e24be4a30b1c86594bf01c666a37712da561b
FeralChild64 <you@example.com>
2019-09-03 18:10:56 +0200

    KickAss adaptation - interrupts and jumptable

--- c64/kernal/interrupts/fe66.default_brk_handler.s
+++ c64/kernal/interrupts/fe66.default_brk_handler.s
@@ -1,9 +1,9 @@
 
 default_brk_handler:
 
-	;; Implemented according to Compute's Mapping the Commodore 64, pages 73-74
+	// Implemented according to Compute's Mapping the Commodore 64, pages 73-74
 
-	sei ; disable IRQs, to be sure they won't interfere
+	sei // disable IRQs, to be sure they won't interfere
 
 	jsr JRESTOR
 	jsr JIOINIT
